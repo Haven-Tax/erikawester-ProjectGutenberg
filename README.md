f5345c2771ca50b52dbfcb66ccabc71a0edec50e
Ari Falkner <ari@tryhaventax.com>
2024-11-07 13:05:17 -0500

    empty

--- README.md
+++
@@ -1,9 +0,0 @@
-Welcome to Project Gutenberg AI Explorer!
-
-This is a simple app that allows you to explore Project Gutenberg books and get an AI summary of the book.
-
-To get started, simply enter a book ID from Project Gutenberg and click the "Fetch Book" button. The app will fetch the book and display it's content and metadata.
-
-You can then get a summary of the book along with the top five main characters.
-
-Enjoy your reading! 📚
--- src/app/utils/supabaseClient.ts
+++
@@ -1,9 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://tgaonwuoxeptkrveauru.supabase.co";
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRnYW9ud3VveGVwdGtydmVhdXJ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA4MzU0NzAsImV4cCI6MjA0NjQxMTQ3MH0.SIsTsocK2LDd8QEjeaW439_QKP9-v_DZMT8Eibp7ARg";
-
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
-
-export default supabase;
